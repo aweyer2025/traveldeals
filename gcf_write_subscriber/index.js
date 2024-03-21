41b354b2f575017dea6ab282124899119714eb08
Anthony Weyer <antweyer@iu.edu>
2024-03-21 23:07:43 +0000

    Write to firestore working

---
+++ gcf_write_subscriber/index.js
@@ -0,0 +1,30 @@
+const {FireStore, Firestore} = require ('@google-cloud/firestore');
+
+
+
+exports.write_subscriber = (message, context) => {
+
+    const incomingMessage = Buffer.from(message.data, 'base64').toString('utf-8');
+
+    const parsedMessage = JSON.parse(incomingMessage);
+
+    console.log(`Decoded message: ${JSON.stringify(parsedMessage)}`);
+    console.log(`Email address: ${parsedMessage.email_address}`);
+
+  async function writeToFs(){
+    const FireStore = new Firestore({
+        projectId: 'sp24-41200-antweyer-traveldeal',
+        databaseId: ''
+    });
+
+    let watchObj = {};
+
+    watchObj.email_address = parsedMessage.email_address
+    watchObj.watch_regions = parsedMessage.watch_region
+
+    //write to firestore
+    let collectionRef = FireStore.collection('subscribers');
+    let documentRef = collectionRef.add(watchObj);
+  }
+  writeToFs()
+}
